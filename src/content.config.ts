84782db73a36f72477b16f4950b3ea705ce4e3b3
Conner Replogle <95052372+conner-replogle@users.noreply.github.com>
2026-04-27 22:05:41 -0500

    fg

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { z, defineCollection, glob } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const metadataDefinition = () =>
   z
